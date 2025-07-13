3f6692c8be9ab95c49d1c001b8d7c2cdd109df63
AntoLotti <antoniolottivillar@gmail.com>
2025-07-13 18:35:53 +0200

    Added: main functions definitions

--- inc/lis3dh_driver.h
+++ inc/lis3dh_driver.h
@@ -6,60 +6,60 @@
 /**************************************************************************
     I2C ADDRESS/BITS (7 bits length; pg. 25/54)
 **************************************************************************/
-#define LIS3DHTR_I2C_DEFAULT_ADDRESS    (0x33 >> 1)  // if SDO/SA0 = 1 -> 00110011, then 00110011 >> 1 = 00011001  
-#define LIS3DHTR_I2C_UPDATED_ADDRESS    (0x18 >> 1)  // if SDO/SA0 = 0 -> 00110001, then 00110001 >> 1 = 00011000  
+#define LIS3DH_I2C_DEFAULT_ADDRESS  (0x33 >> 1) // if SDO/SA0 = 1 -> 00110011, then 00110011 >> 1 = 00011001  
+#define LIS3DH_I2C_UPDATED_ADDRESS  (0x18 >> 1) // if SDO/SA0 = 0 -> 00110001, then 00110001 >> 1 = 00011000  
 
 /**************************************************************************
     CONVERSION DELAY (in mS)
 **************************************************************************/
-#define LIS3DHTR_CONVERSIONDELAY    (100)   //
+#define LIS3DH_CONVERSIONDELAY    (100)   //
 
 /**************************************************************************
     ACCELEROMETER REGISTERS (pg. 31-32/54)
 **************************************************************************/
 // directions [0x00-0x06] reserved
-#define LIS3DHTR_REG_ACCEL_STATUS_REG_AUX   (0x07)  // Status Register Auxiliar
-#define LIS3DHTR_REG_ACCEL_OUT_ADC1_L       (0x08)  // 1-Axis Acceleration Data Low Register
-#define LIS3DHTR_REG_ACCEL_OUT_ADC1_H       (0x09)  // 1-Axis Acceleration Data High Register
-#define LIS3DHTR_REG_ACCEL_OUT_ADC2_L       (0x0A)  // 2-Axis Acceleration Data Low Register
-#define LIS3DHTR_REG_ACCEL_OUT_ADC2_H       (0x0B)  // 2-Axis Acceleration Data High Register
-#define LIS3DHTR_REG_ACCEL_OUT_ADC3_L       (0x0C)  // 3-Axis Acceleration Data Low Register
-#define LIS3DHTR_REG_ACCEL_OUT_ADC3_H       (0x0D)  // 3-Axis Acceleration Data High Register
+#define LIS3DH_REG_ACCEL_STATUS_REG_AUX     (0x07)  // Status Register Auxiliar
+#define LIS3DH_REG_ACCEL_OUT_ADC1_L         (0x08)  // 1-Axis Acceleration Data Low Register
+#define LIS3DH_REG_ACCEL_OUT_ADC1_H         (0x09)  // 1-Axis Acceleration Data High Register
+#define LIS3DH_REG_ACCEL_OUT_ADC2_L         (0x0A)  // 2-Axis Acceleration Data Low Register
+#define LIS3DH_REG_ACCEL_OUT_ADC2_H         (0x0B)  // 2-Axis Acceleration Data High Register
+#define LIS3DH_REG_ACCEL_OUT_ADC3_L         (0x0C)  // 3-Axis Acceleration Data Low Register
+#define LIS3DH_REG_ACCEL_OUT_ADC3_H         (0x0D)  // 3-Axis Acceleration Data High Register
 // direction 0x0E reserved
 #define LIS3DHTR_REG_ACCEL_WHO_AM_I         (0x0F)  // Device identification Register
 // directions [0x10-0x1D] reserved
 #define LIS3DH_CTRL_REG0                    (0x1E)  // 
-#define LIS3DHTR_REG_TEMP_CFG_REG           (0x1F)  // Temperature Sensor Register
-#define LIS3DHTR_REG_ACCEL_CTRL_REG1        (0x20)  // Accelerometer Control Register 1
-#define LIS3DHTR_REG_ACCEL_CTRL_REG2        (0x21)  // Accelerometer Control Register 2
-#define LIS3DHTR_REG_ACCEL_CTRL_REG3        (0x22)  // Accelerometer Control Register 3
-#define LIS3DHTR_REG_ACCEL_CTRL_REG4        (0x23)  // Accelerometer Control Register 4
-#define LIS3DHTR_REG_ACCEL_CTRL_REG5        (0x24)  // Accelerometer Control Register 5
-#define LIS3DHTR_REG_ACCEL_CTRL_REG6        (0x25)  // Accelerometer Control Register 6
-#define LIS3DHTR_REG_ACCEL_REFERENCE        (0x26)  // Reference/Datacapture Register
-#define LIS3DHTR_REG_ACCEL_STATUS_REG       (0x27)  // Status Register
-#define LIS3DHTR_REG_ACCEL_OUT_X_L          (0x28)  // X-Axis Acceleration Data Low Register
-#define LIS3DHTR_REG_ACCEL_OUT_X_H          (0x29)  // X-Axis Acceleration Data High Register
-#define LIS3DHTR_REG_ACCEL_OUT_Y_L          (0x2A)  // Y-Axis Acceleration Data Low Register
-#define LIS3DHTR_REG_ACCEL_OUT_Y_H          (0x2B)  // Y-Axis Acceleration Data High Register
-#define LIS3DHTR_REG_ACCEL_OUT_Z_L          (0x2C)  // Z-Axis Acceleration Data Low Register
-#define LIS3DHTR_REG_ACCEL_OUT_Z_H          (0x2D)  // Z-Axis Acceleration Data High Register
-#define LIS3DHTR_REG_ACCEL_FIFO_CTRL        (0x2E)  // FIFO Control Register
-#define LIS3DHTR_REG_ACCEL_FIFO_SRC         (0x2F)  // FIFO Source Register
-#define LIS3DHTR_REG_ACCEL_INT1_CFG         (0x30)  // Interrupt 1 Configuration Register
-#define LIS3DHTR_REG_ACCEL_INT1_SRC         (0x31)  // Interrupt 1 Source Register
-#define LIS3DHTR_REG_ACCEL_INT1_THS         (0x32)  // Interrupt 1 Threshold Register
-#define LIS3DHTR_REG_ACCEL_INT1_DURATION    (0x33)  // Interrupt 1 Duration Register
-#define LIS3DHTR_REG_ACCEL_INT2_CFG         (0x34)  // Interrupt 2 Configuration Register
-#define LIS3DHTR_REG_ACCEL_INT2_SRC         (0x35)  // Interrupt 2 Source Register
-#define LIS3DHTR_REG_ACCEL_INT2_THS         (0x36)  // Interrupt 2 Threshold Register
-#define LIS3DHTR_REG_ACCEL_INT2_DURATION    (0x37)  // Interrupt 2 Duration Register
-#define LIS3DHTR_REG_ACCEL_CLICK_CFG        (0x38)  // Interrupt Click Recognition Register
-#define LIS3DHTR_REG_ACCEL_CLICK_SRC        (0x39)  // Interrupt Click Source Register
-#define LIS3DHTR_REG_ACCEL_CLICK_THS        (0x3A)  // Interrupt Click Threshold Register
-#define LIS3DHTR_REG_ACCEL_TIME_LIMIT       (0x3B)  // Click Time Limit Register
-#define LIS3DHTR_REG_ACCEL_TIME_LATENCY     (0x3C)  // Click Time Latency Register
-#define LIS3DHTR_REG_ACCEL_TIME_WINDOW      (0x3D)  // Click Time Window Register
+#define LIS3DH_REG_TEMP_CFG_REG             (0x1F)  // Temperature Sensor Register
+#define LIS3DH_REG_ACCEL_CTRL_REG1          (0x20)  // Accelerometer Control Register 1
+#define LIS3DH_REG_ACCEL_CTRL_REG2          (0x21)  // Accelerometer Control Register 2
+#define LIS3DH_REG_ACCEL_CTRL_REG3          (0x22)  // Accelerometer Control Register 3
+#define LIS3DH_REG_ACCEL_CTRL_REG4          (0x23)  // Accelerometer Control Register 4
+#define LIS3DH_REG_ACCEL_CTRL_REG5          (0x24)  // Accelerometer Control Register 5
+#define LIS3DH_REG_ACCEL_CTRL_REG6          (0x25)  // Accelerometer Control Register 6
+#define LIS3DH_REG_ACCEL_REFERENCE          (0x26)  // Reference/Datacapture Register
+#define LIS3DH_REG_ACCEL_STATUS_REG         (0x27)  // Status Register
+#define LIS3DH_REG_ACCEL_OUT_X_L            (0x28)  // X-Axis Acceleration Data Low Register
+#define LIS3DH_REG_ACCEL_OUT_X_H            (0x29)  // X-Axis Acceleration Data High Register
+#define LIS3DH_REG_ACCEL_OUT_Y_L            (0x2A)  // Y-Axis Acceleration Data Low Register
+#define LIS3DH_REG_ACCEL_OUT_Y_H            (0x2B)  // Y-Axis Acceleration Data High Register
+#define LIS3DH_REG_ACCEL_OUT_Z_L            (0x2C)  // Z-Axis Acceleration Data Low Register
+#define LIS3DH_REG_ACCEL_OUT_Z_H            (0x2D)  // Z-Axis Acceleration Data High Register
+#define LIS3DH_REG_ACCEL_FIFO_CTRL          (0x2E)  // FIFO Control Register
+#define LIS3DH_REG_ACCEL_FIFO_SRC           (0x2F)  // FIFO Source Register
+#define LIS3DH_REG_ACCEL_INT1_CFG           (0x30)  // Interrupt 1 Configuration Register
+#define LIS3DH_REG_ACCEL_INT1_SRC           (0x31)  // Interrupt 1 Source Register
+#define LIS3DH_REG_ACCEL_INT1_THS           (0x32)  // Interrupt 1 Threshold Register
+#define LIS3DH_REG_ACCEL_INT1_DURATION      (0x33)  // Interrupt 1 Duration Register
+#define LIS3DH_REG_ACCEL_INT2_CFG           (0x34)  // Interrupt 2 Configuration Register
+#define LIS3DH_REG_ACCEL_INT2_SRC           (0x35)  // Interrupt 2 Source Register
+#define LIS3DH_REG_ACCEL_INT2_THS           (0x36)  // Interrupt 2 Threshold Register
+#define LIS3DH_REG_ACCEL_INT2_DURATION      (0x37)  // Interrupt 2 Duration Register
+#define LIS3DH_REG_ACCEL_CLICK_CFG          (0x38)  // Interrupt Click Recognition Register
+#define LIS3DH_REG_ACCEL_CLICK_SRC          (0x39)  // Interrupt Click Source Register
+#define LIS3DH_REG_ACCEL_CLICK_THS          (0x3A)  // Interrupt Click Threshold Register
+#define LIS3DH_REG_ACCEL_TIME_LIMIT         (0x3B)  // Click Time Limit Register
+#define LIS3DH_REG_ACCEL_TIME_LATENCY       (0x3C)  // Click Time Latency Register
+#define LIS3DH_REG_ACCEL_TIME_WINDOW        (0x3D)  // Click Time Window Register
 #define LIS3DH_ACT_THS                      (0x3E)  // 
 #define LIS3DH_ACT_DUR                      (0x3F)  // 
 
@@ -72,5 +72,60 @@ typedef struct lis3dh_s
     float acc_mps2[3];              // Acceleration data [X,Y,Z] in m/s^2
     float temp_deg;                 // Temperature data in degreds 
 }lis3dh_t;
+
+/**************************************************************************
+    FUNCTIONS DEFINITIONS
+**************************************************************************/
+
+/**
+ * @brief Initialize of the peripheral
+ * @author Antonio Lotti Villar ()
+ * @date 12/07/2025
+ * @param dst A pointer to the lis3dh structure
+ * @param i2cHandle A pointer to the I2C handle
+ */
+uint8_t lis3dh_init( lis3dh_t *dst, I2C_HandleTypeDef *i2cHandle );
+
+/**
+ * @brief Read the accelerometor data from the peripheral
+ * @author Antonio Lotti Villar ()
+ * @param dst A pointer to the lis3dh structure
+ */
+HAL_StatusTypeDef lis3dh_read_acc( lis3dh_t *dst );
+
+/**
+ * @brief Read the temperature data from the peripheral
+ * @author Antonio Lotti Villar ()
+ * @param dst A pointer to the lis3dh structure
+ */
+HAL_StatusTypeDef lis3dh_read_temp( lis3dh_t *dst);
+
+/**
+ * @brief Read a register from the peripheral
+ * @author Antonio Lotti Villar ()
+ * @param dst A pointer to the lis3dh structure
+ * @param reg Register address to read
+ * @param data Pointer where the data will be store
+ */
+HAL_StatusTypeDef lis3dh_read_reg( lis3dh_t *dst, uint8_t reg, uint8_t *data );
+
+/**
+ * @brief Read a register from the peripheral
+ * @author Antonio Lotti Villar ()
+ * @param dst A pointer to the lis3dh structure
+ * @param reg Register address to start reading
+ * @param data Pointer where the data will be store
+ * @param length How many registers will be readed 
+ */
+HAL_StatusTypeDef lis3dh_read_regs( lis3dh_t *dst, uint8_t reg, uint8_t *data, uint16_t length );
+
+/**
+ * @brief Read a register from the peripheral
+ * @author Antonio Lotti Villar ()
+ * @param dst A pointer to the lis3dh structure
+ * @param reg Register address to write
+ * @param data Pointer to the data that will be writen in the registers
+ */
+HAL_StatusTypeDef lis3dh_write_reg( lis3dh_t *dst, uint8_t reg, uint8_t *data);
 
 #endif /*LIS3DH_DRIVER_H*/
